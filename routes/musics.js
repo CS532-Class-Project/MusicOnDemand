560c84cc96e0cb512dd6e52f021735877fb5eb05
Yuxi <55152141+szwyx1999@users.noreply.github.com>
2023-05-05 19:08:34 -0600

    update external with sample data and create page

--- routes/musics.js
+++ routes/musics.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Music = require('../models/Music')
+const Music = require('../models/music')
 
 // Get all the musics
 router.get('/list', async (req, res) => {
