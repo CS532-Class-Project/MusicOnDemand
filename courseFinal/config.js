d3085a4a0edd097129a4e0b62b60cd239a5b48ae
Patrick Silva <44123232+jpatsilva@users.noreply.github.com>
2023-05-07 22:18:27 -0600

    Delete courseFinal directory
    Removed unwanted code

--- courseFinal/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  googleKey: "800464719802-pecr0t7lnj9kpntdc419050h5361cskt.apps.googleusercontent.com",
-  googleSecret: "q2SaOGNvCSZZ3rHFeVO6syAc"
-};
--- courseFinal/node_modules/opencv4nodejs-prebuilt-install/lib/bundle.js
+++
@@ -1,8 +0,0 @@
-const { opencvLibDir } = require("./dirs");
-const { readdirSync } = require("fs");
-const { resolvePath } = require('./commons');
-const { join } = require("path");
-
-for (const file of readdirSync(opencvLibDir)) {
-    console.log(resolvePath(join(opencvLibDir, file)));
-}
